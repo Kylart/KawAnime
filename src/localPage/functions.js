1b020e897a82d2e2424bb7ef5582916632268107
Kylart <garnier@et.esiea.fr>
2017-03-04 21:08:05 +0100

    UI of local page fully implemented. Going to make open and delete functions

--- src/localPage/functions.js
+++ src/localPage/functions.js
@@ -1,8 +1,85 @@
 /**
- * Created by Kylart on 01/03/2017.
+ * Created by Kylart on 04/03/2017.
  *
  * In this file, all the functions needed by the local page
  * are present. This is for cleaner code.
  *
  */
 
+const self = this
+
+// This will be later set with the config file
+const downloadRep = 'Downloads'
+
+const fs = require('fs')
+const path = require('path')
+const os = require('os')
+
+const mal = require('malapi').Anime
+
+const DIR = path.join(os.userInfo().homedir, downloadRep)
+
+exports.filterFiles = (files, filters) => {
+  let filteredFiles = []
+
+  for (let filter in filters)
+  {
+    files.forEach((file) => {
+      if (path.extname(file) === filters[filter])
+        filteredFiles.push(file)
+    })
+  }
+
+  return filteredFiles
+}
+
+exports.byProperty = (prop) => {
+  return function (a, b) {
+    if (typeof a[prop] == "number")
+    {
+      return (a[prop] - b[prop])
+    }
+    return ((a[prop] < b[prop]) ? -1 : ((a[prop] > b[prop]) ? 1 : 0))
+  }
+}
+
+// This function works only if the file is named this way:
+// [FANSUB] <NAME> <-> <EP_NUMBER> <EXTENSION_NAME>
+exports.searchAnime = (filename, object) => {
+  let epNumber = 0
+
+  filename = filename.split(' ')
+  for (let i = 0; i < 3; ++i) i === 1 ? epNumber = filename.pop()
+                                      : filename.pop()
+
+  filename = filename.slice(1).join(' ')
+
+  let result = {
+    title: filename,
+    episode: epNumber
+  }
+
+  mal.fromName(filename).then((anime) => {
+    result.picture = anime.image
+    result.synopsis = anime.synopsis.slice(0, 200) + '...'
+    result.numberOfEpisodes = anime.episodes.replace('Unknown', 'NC')
+    result.status = anime.status
+    result.year = anime.aired.split(' ')[2]
+    result.genres = anime.genres.join(', ')
+    result.classification = anime.classification
+    result.mark = anime.statistics.score.value
+
+    object.files.push(result)
+    object.files.sort(self.byProperty('title'))
+  })
+}
+
+exports.findFiles = (object) => {
+  const allFiles = fs.readdirSync(DIR)
+
+  const filteredFiles = self.filterFiles(allFiles, ['.mkv', '.mp4'])
+
+  filteredFiles.forEach((file) => {
+    self.searchAnime(file, object)
+  })
+}
--- src/localPage/index.js
+++ src/localPage/index.js
@@ -10,19 +10,95 @@ const functions = require('./functions.js')
 
 const html = `
 <div>
-  <h1>{{ message }}</h1>
+  <div class="mdl-grid">
+    <template v-for="file in files">
+      <div class="mdl-cell mdl-cell--6-col local-elem" v-bind:style="elemStyle">
+        <div class="mdl-grid local-info">
+          <div class="mdl-cell mdl-cell--7-col">
+            <div class="local-title text-ellipsis">
+               <h6 class="title-text" v-bind:title="file.title">{{ file.title }}</h6>
+            </div>
+          </div>
+          <div class="mdl-cell mdl-cell--2-col">
+            <div class="local-episodes title-text">
+            <h6 class="ep-number-text">
+              {{ file.episode }}/{{ file.numberOfEpisodes }}
+             </h6>
+            </div>
+          </div>
+          <div class="mdl-cell mdl-cell--3-col">
+            <div class="local-page-buttons">
+              <md-button class="md-icon-button play-button" @click.native=""
+                         v-bind:style="playButtonStyle">
+                <i class="mdi mdi-play-circle-outline mdi-36px"></i>
+              </md-button>
+              <md-button class="md-icon-button play-button" @click.native=""
+                         v-bind:style="delButtonStyle">
+                <i class="mdi mdi-delete-empty mdi-24px"></i>
+              </md-button>
+            </div>
+          </div>
+          <div class="mdl-cell mdl-cell--8-col">
+            <p class="genres-text text-ellipsis">{{ file.genres }}</p>
+          </div>
+          <div class="mdl-cell mdl-cell--4-col">
+            <p class="classification-text text-ellipsis"
+               v-bind:title="file.classification">{{ file.classification }}</p>
+          </div>
+          <div class="local-pic-synopsis-container">
+            <div class="local-picture">
+              <img v-bind:src="file.picture">
+            </div>
+            <div class="synopsis-year-container">
+              <div class="local-synopsis">
+                {{ file.synopsis }}
+              </div>
+              <div class="local-year-container">
+                <div class="local-year">
+                  <div class="local-status">
+                    {{ file.status }}
+                  </div>
+                  <div class="local-year-text">
+                    {{ file.year }}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </template>
+  </div>
 </div>`
 
 Vue.component('local-page', {
   template: html,
   data: function () {
     return {
-      message: "Hello"
+      files: [],
+      elemStyle: {
+        marginBottom: '1.5%'
+      },
+      playButtonStyle: {
+        paddingLeft: '2px',
+        marginRight: '0px',
+        marginLeft: '0px'
+      },
+      delButtonStyle: {
+        paddingLeft: '8px',
+        marginLeft: '0px'
+      }
     }
   },
   methods: {
     findFiles: function () {
-      return functions.findFiles()
+      return functions.findFiles(this)
+    },
+    test: function () {
+      console.log("Hello")
     }
+  },
+  created: function () {
+    this.findFiles()
   }
 })
