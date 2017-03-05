0c53e39ecab4b50f68c7b524a6c72c8571dd5f6f
Kylart <garnier@et.esiea.fr>
2017-03-05 18:20:34 +0100

    WatchList page Fully implemented. Still have to work on download button though.

--- src/watchList/functions.js
+++ src/watchList/functions.js
@@ -5,3 +5,103 @@
  * are present. This is for cleaner code.
  *
  */
+
+const os = require('os')
+const path = require('path')
+const fs = require('fs')
+
+const DIR = path.join(os.userInfo().homedir, '.KawAnime')
+const listsPath = path.join(DIR, 'lists.json')
+
+const self = this
+
+exports.loadLists = (object) => {
+  let lists = fs.readFileSync(listsPath, 'utf-8', (err) => {
+    if (err) throw err
+  })
+
+  lists = JSON.parse(lists)
+
+  object.seeing = lists.seeing
+  object.seen = lists.seen
+  object.watchList = lists.watchList
+}
+
+exports.saveLists = (object) => {
+  let json = JSON.stringify({
+    "seeing": object.seeing,
+    "seen": object.seen,
+    "watchList": object.watchList
+  })
+
+  fs.writeFileSync(listsPath, json, 'utf-8', (err) => {
+    if (err) throw err
+  })
+}
+
+exports.alreadyExists = (name, checkList) => {
+  for (let i = 0; i < checkList.length; ++i)
+    if (checkList[i] === name) return true
+
+  return false
+}
+
+exports.addItem = (object, listName) => {
+  // Here, the semi are needed!
+  switch (listName)
+  {
+    case 'seeing':
+      if (object.seeingEntry !== '' && !self.alreadyExists(object.seeingEntry, object.seeing))
+      {
+        object.seeing.push(object.seeingEntry);
+        object.seeingEntry = '';
+      }
+
+      break
+
+    case 'seen':
+      if (object.seenEntry !== '' && !self.alreadyExists(object.seenEntry, object.seen))
+      {
+        object.seen.push(object.seenEntry);
+        object.seenEntry = '';
+      }
+
+      break
+
+    case 'watchList':
+      if (object.watchListEntry !== '' && !self.alreadyExists(object.watchListEntry, object.watchList))
+      {
+        object.watchList.push(object.watchListEntry);
+        object.watchListEntry = '';
+      }
+
+      break
+  }
+}
+
+exports.downloadThis = (name) => {
+
+}
+
+exports.delItem = (object, name, listName) => {
+  switch (listName)
+  {
+    case 'seeing':
+      for (let i = 0; i < object.seeing.length; ++i)
+        if (object.seeing[i] === name) object.seeing.splice(i, 1)
+
+      break
+
+    case 'seen':
+      for (let i = 0; i < object.seen.length; ++i)
+        if (object.seen[i] === name) object.seen.splice(i, 1)
+
+      break
+
+    case 'watchList':
+      for (let i = 0; i < object.watchList.length; ++i)
+        if (object.watchList[i] === name) object.watchList.splice(i, 1)
+
+      break
+  }
+}
--- src/watchList/index.js
+++ src/watchList/index.js
@@ -8,42 +8,124 @@
 
 const functions = require('./functions.js')
 
+const seeingHtml = `
+<div class="mdl-cell mdl-cell--6-col list">
+  <div class="mdl-grid">
+    <div class="mdl-cell mdl-cell--12-col list-title"><h4>Seeing</h4></div>
+    <div class="mdl-cell mdl-cell--12-col entry-button">
+      <form novalidate @submit.stop.prevent="addItem('seeing')">
+        <md-input-container v-bind:style="entryStyle">
+          <label>New entry</label>
+          <md-input @keydown.enter="addItem('seeing')" 
+          v-model="seeingEntry"
+          style="text-shadow: 0 0 0 rgba(255, 255, 255, 0.8);"></md-input>
+        </md-input-container>
+      </form>
+    </div>
+    <div class="mdl-cell mdl-cell--12-col entry-container" v-for="anime in seeing">
+      <div class="mdl-grid entry">
+        <div class="mdl-cell mdl-cell--8-col entry-name">
+          <h6 class="text-ellipsis" v-bind:title="anime">{{ anime }}</h6>
+        </div>
+        <div class="mdl-cell mdl-cell--4-col entry-options">
+          <md-button class="md-icon-button dl-button" 
+             @click.native=""
+             v-bind:style="dlButtonStyle">
+            <i class="mdi mdi-download mdi-36px"></i>
+          </md-button>
+          <md-button class="md-icon-button play-button" 
+             @click.native="delItem(anime, 'seeing')"
+             v-bind:style="delButtonStyle">
+            <i class="mdi mdi-delete-empty mdi-24px"></i>
+          </md-button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>`
+
+const seenHtml = `
+<div class="mdl-cell mdl-cell--6-col list">
+  <div class="mdl-grid">
+    <div class="mdl-cell mdl-cell--12-col list-title"><h4>Seen</h4></div>
+    <div class="mdl-cell mdl-cell--12-col entry-button">
+      <form novalidate @submit.stop.prevent="addItem('seen')">
+        <md-input-container v-bind:style="entryStyle">
+          <label>New entry</label>
+          <md-input @keydown.enter="addItem('seen')" 
+          v-model="seenEntry"
+          style="text-shadow: 0 0 0 rgba(255, 255, 255, 0.8);"></md-input>
+        </md-input-container>
+      </form>
+    </div>
+    <div class="mdl-cell mdl-cell--12-col entry-container" v-for="anime in seen">
+      <div class="mdl-grid entry">
+        <div class="mdl-cell mdl-cell--8-col entry-name">
+          <h6 class="text-ellipsis" v-bind:title="anime">{{ anime }}</h6>
+        </div>
+        <div class="mdl-cell mdl-cell--4-col entry-options">
+          <md-button class="md-icon-button dl-button" 
+             @click.native=""
+             v-bind:style="dlButtonStyle">
+            <i class="mdi mdi-download mdi-36px"></i>
+          </md-button>
+          <md-button class="md-icon-button del-button" 
+             @click.native="delItem(anime, 'seen')"
+             v-bind:style="delButtonStyle">
+            <i class="mdi mdi-delete-empty mdi-24px"></i>
+          </md-button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>`
+
+const watchListHtml = `
+<div class="mdl-cell mdl-cell--12-col watch-list">
+  <div class="mdl-cell mdl-cell--12-col list-title"><h4>Watch List</h4></div>
+  <div class="mdl-grid">
+    <div class="mdl-cell mdl-cell--12-col entry-button">
+      <form novalidate @submit.stop.prevent="addItem('watchList')">
+        <md-input-container v-bind:style="watchListEntryStyle">
+          <label>New entry</label>
+          <md-input @keydown.enter="addItem('watchList')" 
+          v-model="watchListEntry"
+          style="text-shadow: 0 0 0 rgba(255, 255, 255, 0.8);"></md-input>
+        </md-input-container>
+      </form>
+    </div>
+    <div class="mdl-cell mdl-cell--6-col entry-container" v-for="anime in watchList">
+      <div class="mdl-grid entry">
+        <div class="mdl-cell mdl-cell--8-col entry-name">
+          <h6 class="text-ellipsis" v-bind:title="anime">{{ anime }}</h6>
+        </div>
+        <div class="mdl-cell mdl-cell--4-col entry-options">
+          <md-button class="md-icon-button dl-button" 
+             @click.native=""
+             v-bind:style="dlButtonStyle">
+            <i class="mdi mdi-download mdi-36px"></i>
+          </md-button>
+          <md-button class="md-icon-button del-button" 
+             @click.native="delItem(anime, 'watchList')"
+             v-bind:style="delButtonStyle">
+            <i class="mdi mdi-delete-empty mdi-24px"></i>
+          </md-button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>`
+
 const html = `
 <div>
   <!--TODO: Wait for material-vue 0.8.0 to make md-tabs background color as wanted.-->
   <!--See renanhangai's comment https://github.com/marcosmoura/vue-material/issues/202-->
   <md-theme md-name="watchList">
-    <md-tabs md-fixed>
-      <md-tab id="anime-tab" md-label="Animes">
         <div class="mdl-grid">
-          <div class="mdl-cell mdl-cell--6-col">
-            <div class="mdl-grid">
-              <div class="dummy-cell mdl-cell mdl-cell--4-col"></div>
-              <div class="mdl-cell mdl-cell--8-col">
-                
-              </div>
-              <div class="mdl-cell mdl-cell--6-col" 
-                   v-for="anime in seeing">
-                {{ anime }}
-              </div>
-            </div>
-          </div>
-          <div class="mdl-cell mdl-cell--6-col">
-            <div class="mdl-grid">
-              <div class="mdl-cell mdl-cell--6-col" 
-                   v-for="anime in seeing">
-                {{ anime }}
-              </div>
-            </div>
-          </div>
+          ${watchListHtml}
+          ${seeingHtml}
+          ${seenHtml}
         </div>
-      </md-tab>
-    
-      <md-tab id="movie-tab" md-label="Movies">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolorum quas amet cum vitae, omnis! Illum quas voluptatem, expedita iste, dicta ipsum ea veniam dolore in, quod saepe reiciendis nihil.</p>
-      </md-tab>
-    </md-tabs>
   </md-theme>
 </div>
 `
@@ -52,12 +134,40 @@ Vue.component('watch-list', {
   template: html,
   data: function () {
     return {
-      seeing: ['sdgf', 'agg', 'agg', 'agg', 'agg', 'agg', 'agg'],
-      seen: ['agg', 'agg', 'agg', 'agg', 'agg', 'agg', 'agg', 'agg'],
-      watchList: []
+      seeing: [],
+      seen: [],
+      watchList: [],
+      seeingEntry: '',
+      seenEntry: '',
+      watchListEntry: '',
+      entryStyle: {
+        width: '60%'
+      },
+      watchListEntryStyle: {
+        width: '40%'
+      },
+      dlButtonStyle: {
+        paddingLeft: '2px',
+        marginRight: '0px',
+        marginLeft: '0px'
+      },
+      delButtonStyle: {
+        paddingLeft: '8px',
+        marginLeft: '0px'
+      }
     }
   },
-  methods: function () {
-
+  methods: {
+    addItem: function (listName) {
+      functions.addItem(this, listName)
+      functions.saveLists(this)
+    },
+    delItem: function (name, listName) {
+      functions.delItem(this, name, listName)
+      functions.saveLists(this)
+    }
+  },
+  created: function () {
+    functions.loadLists(this)
   }
 })
