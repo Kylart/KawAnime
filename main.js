641ad50c5901953122809e1cba985b61d5e14c23
Kylart <garnier@et.esiea.fr>
2017-03-11 18:50:14 +0100

    Added vuejs devtools.

--- main.js
+++ main.js
@@ -62,7 +62,12 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () => {
+  createWindow()
+  if (process.env.NODE_ENV !== 'production') {
+    require('vue-devtools').install()
+  }
+})
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
