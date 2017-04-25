12fb4eb2fd28ce636fe2040df245bb7df71382e2
Kylart <garnier@et.esiea.fr>
2017-04-26 00:27:27 +0200

    Now builds DMG.

--- assets/scripts/dist.js
+++ assets/scripts/dist.js
@@ -18,7 +18,12 @@
 
 const {join} = require('path')
 
+// packages
 const packager = require('electron-packager')
+const materialPath = join(__dirname, '..', 'build', 'material')
+
+// Installers
+const createDMG = require('electron-installer-dmg')
 
 const colors = require('colors')
 
@@ -28,18 +33,46 @@ const packOptions = {
   out: join(__dirname, '..', 'build', 'dists'),
   appCopyright: `© 2016 - ${(new Date()).getYear() + 1900} Kylart`,
   all: true,
-  icon: join(__dirname, '..', 'build', 'material', 'icon'),
+  icon: join(materialPath, 'icon'),
   overwrite: true
   // Check for signing app
 }
 
-const buildDists = () => {
+const dmgOptions = {
+  appPath: join(materialPath, '..', 'dists', 'KawAnime-darwin-x64', 'KawAnime.app'),
+  name: 'KawAnime',
+  background: join(materialPath, 'background.png'),
+  icon: join(materialPath, 'icon.icns'),
+  overwrite: true,
+  out: join(materialPath, '..', 'dists'),
+  "icon-size": 80
+}
 
+const makeDMG = () => {
+  console.log('[Builder]: Creating DMG...'.yellow)
+
+  createDMG(dmgOptions, function done(err) {
+    if (err)
+    {
+      console.log('[Builder]: An error occurred while creating DMG.'.red)
+      throw err
+    }
+
+    console.log('[Builder]: Successfully built DMG!'.green)
+  })
+}
+
+const buildDists = () => {
+  makeDMG()
 }
 
 const pack = () => {
   packager(packOptions, function done_callback(err, appPaths) {
-    if (err) throw err
+    if (err)
+    {
+      console.log('[Builder]: An occurred while packaging KawAnime!'.red)
+      throw err
+    }
 
     appPaths.forEach((path) => {
       console.log(`[Builder]: Successfully built ${path}!`.green)
