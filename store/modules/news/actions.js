7983a43233c70245d2a4a00efac943e1c497dc55
Kylart <Kylart.dev@gmail.com>
2017-11-27 22:35:33 +0100

    fix news actions

--- store/modules/news/actions.js
+++ store/modules/news/actions.js
@@ -1,7 +1,7 @@
 import {axios, log} from '../../utils'
 
 export default {
-  async init ({state, commit, dispatch}) {
+  async init ({commit, dispatch}) {
     console.log('[INIT] News')
 
     try {
@@ -11,11 +11,11 @@ export default {
         commit('set', data)
       } else {
         log('A problem occurred while gathering the news.')
-        setTimeout(() => { dispatch('init') })
+        setTimeout(() => { dispatch('init') }, 30 * 1000)
       }
     } catch (e) {
       log('A problem occurred while gathering the news.')
-      setTimeout(() => { dispatch('init') })
+      setTimeout(() => { dispatch('init') }, 30 * 1000)
     }
   },
   async refresh ({commit, dispatch}) {
@@ -27,15 +27,16 @@ export default {
 
     status === 200
       ? commit('set', data)
-      : log('A problem occurred while gathering the news.') && dispatch('refresh')
+      : log('A problem occurred while gathering the news.') && setTimeout(() => { dispatch('refresh') }, 30 * 1000)
   },
   async openLink ({rootState}, link) {
-    log(`Opening ${link}`)
+    log(`Opening ${link}.`)
 
-    await axios.get(
-      rootState.config.config.inside
-        ? `openThis?type=insideLink&link=${link}`
-        : `openThis?type=link&link=${link}`
-    )
+    await axios.get('openThis', {
+      params: {
+        type: rootState.config.config.inside ? 'inside' : 'link',
+        link
+      }
+    })
   }
 }
