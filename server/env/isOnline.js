ac714ef485f23a7f968d5ba7e7218a0a0254d583
Kylart <Kylart.dev@gmail.com>
2018-05-02 22:29:03 +0200

    isOnline check should ref app

--- server/env/isOnline.js
+++ server/env/isOnline.js
@@ -1,7 +1,11 @@
 const axios = require('axios')
 
 const isOnline = (req, res) => {
-  axios.get('https://kawanime.com')
+  axios.get('https://kawanime.com', {
+    params: {
+      ref: 'app'
+    }
+  })
     .then(({status}) => {
       res.status(status === 200 ? 200 : 204).send()
     })
