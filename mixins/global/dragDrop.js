3596ae6e078b26521a86a2f9d95acbf5db3ea391
Kylart <Kylart.dev@gmail.com>
2018-05-21 09:44:14 +0200

    Fix drag & drop behavior. Now handling events without external package

--- mixins/global/dragDrop.js
+++ mixins/global/dragDrop.js
@@ -1,9 +1,9 @@
-import dragDrop from 'drag-drop'
-
 export default {
   data: () => ({
-    el: '#app',
-    overlayEl: '#overlay'
+    el: 'app',
+    canEnter: true,
+    videoExtension: ['mkv', 'mp4', 'avi'],
+    torrentExtension: ['torrent']
   }),
 
   computed: {
@@ -18,22 +18,48 @@ export default {
   },
 
   mounted () {
-    const vm = this
+    const elem = document.getElementById(this.el)
 
-    dragDrop(this.el, {
+    elem.ondragenter = this.onDragEnter
+    elem.ondragleave = this.onDragLeave
+    elem.ondragover = this.onDragOver
+    elem.ondrop = this.onDrop
+  },
 
-      onDragEnter () {
-        vm.overlay = true
+  methods: {
+    showOverlay (bool) {
+      this.overlay = bool
+
+      if (!bool) this.canEnter = true
+    },
+    onDragEnter (e) {
+      if (this.canEnter) {
+        this.showOverlay(true)
       }
-    })
+    },
+    onDragLeave (e) {
+      const { screenX, screenY, clientX, clientY } = e
 
-    dragDrop(this.overlayEl, {
-      onDrop (files /*, pos, fileList */) {
-        const { name: fullName, path } = files[0]
+      if (screenX === 0 && screenY === 0 && clientX === 0 && clientY === 0) {
+        this.showOverlay(false)
+      }
+    },
+    onDragOver (e) {
+      e.preventDefault()
+    },
+    onDrop (e) {
+      e.preventDefault()
+      e.stopPropagation()
 
+      const { dataTransfer: { files: [file] } } = e
+      const { name: fullName, path } = file
+
+      const fileExtension = path.split('.').slice(-1)[0]
+
+      if (this.torrentExtension.includes(fileExtension) || this.videoExtension.includes(fileExtension)) {
         const name = fullName.split(' ').slice(1, -1).join(' ') // nyanparser pls
 
-        vm.$store.commit('streaming/play', {
+        this.$store.commit('streaming/play', {
           show: true,
           link: {
             link: path,
@@ -49,11 +75,8 @@ export default {
           })
         }
 
-        vm.overlay = false
-      },
-      onDragLeave () {
-        vm.overlay = false
+        this.showOverlay(false)
       }
-    })
+    }
   }
 }
