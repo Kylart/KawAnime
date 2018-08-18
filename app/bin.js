d0a1c9e19fe6d5e5fbe7f94c6038cf4c1c60380d
Kylart <Kylart.dev@gmail.com>
2018-08-18 16:11:40 +0200

    Removed start command for command line

--- app/bin.js
+++ app/bin.js
@@ -6,27 +6,24 @@ const { join } = require('path')
 const args = require('args')
 const { port } = require('./config')
 
-function start (name, sub, opts) {
-  process.env.PORT = opts.port
-  process.env.KAWANIME_SERVER = opts.server
-  process.env.VERBOSE = opts.verbose
-
-  if (!opts.server) {
-    const electronProcess = require(join(__dirname, 'electron', 'runner.js'))(opts)
-    console.log(`> App started with pid ${electronProcess.pid}.`)
-  } else {
-    require(__dirname)
-  }
-}
-
 function init () {
   args
     .option('port', 'The port on which the app will be running', port)
     .option('server', 'If set to true, KawAnime will start in server-only mode', false)
     .option('verbose', 'Whether or not you want to see logs', false)
-    .command('start', 'Start the app', start)
 
-  args.parse(process.argv)
+  const flags = args.parse(process.argv)
+
+  process.env.PORT = flags.port
+  process.env.KAWANIME_SERVER = flags.server
+  process.env.VERBOSE = flags.verbose
+
+  if (!flags.server) {
+    const electronProcess = require(join(__dirname, 'electron', 'runner.js'))(flags)
+    console.log(`> App started with pid ${electronProcess.pid}.`)
+  } else {
+    require(__dirname)
+  }
 }
 
 init()
