c1f7398f32b66cf76af74975d22d201e36c585ce
Kylart <Kylart.dev@gmail.com>
2018-11-10 19:43:57 +0100

    Reduced default config in vuex config to minimum

--- app/web/store/modules/config/state.js
+++ app/web/store/modules/config/state.js
@@ -2,25 +2,9 @@ import * as Lists from '../lists.js'
 
 export default {
   config: {
-    fansub: 'HorribleSubs',
-    quality: '720p',
-    feed: 'si',
-    localPath: '',
-    sound: 'Nyanpasu',
-    notificationVolume: 0.5,
-    inside: true,
-    magnets: false,
-    system: {
-      autoStart: false,
-      toTray: false
-    },
-    video: {
-      inside: true,
-      autoplay: true,
-      fullscreen: false,
-      preferredLanguage: 'en',
-      quality: '720p'
-    }
+    bounds: {},
+    system: {},
+    video: {}
   },
   ...Lists
 }
