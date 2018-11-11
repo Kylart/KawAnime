280bd226500595e7b5207e837399ebf64e1285fc
Kylart <Kylart.dev@gmail.com>
2018-11-11 18:00:39 +0100

    Updated to buttercup 2.7

--- app/api/vault/index.js
+++ app/api/vault/index.js
@@ -1,8 +1,10 @@
 const { join } = require('path')
 const { writeFileSync, readFileSync, existsSync, mkdirSync } = require('fs')
 const randomString = require('randomstring')
+
 const buttercup = require('buttercup')
-const { Archive, createCredentials, FileDatasource, EntryFinder } = buttercup
+const { Archive, Credentials, Datasources } = buttercup
+const { FileDatasource } = Datasources
 
 const { Logger, dir } = require('../utils')
 const logger = new Logger('Vault')
@@ -10,7 +12,7 @@ const logger = new Logger('Vault')
 const DIR = join(dir, 'vault')
 const keyPath = join(DIR, 'p')
 
-const setupCreds = (service, credentials) => {
+const setupCreds = (service, creds) => {
   return new Promise((resolve, reject) => {
     /* istanbul ignore next */
     if (!existsSync(DIR)) {
@@ -19,23 +21,25 @@ const setupCreds = (service, credentials) => {
       logger.info('No vault detected. Creating...')
 
       // Setting up master key
+      // This should be a separate method linked to a route
+      // so that the user can set its own master password
+      // if they want to.
       writeFileSync(keyPath, randomString.generate(40), 'utf-8')
     }
 
-    // Setting up archive for this service
-    const archive = new Archive()
-
-    const main = archive.createGroup('main')
-    const entry = main.createEntry('account')
-
-    entry
-      .setAttribute('username', credentials.username)
-      .setAttribute('password', credentials.password)
-
-    const ds = new FileDatasource(join(DIR, service + '.bcup'))
+    const fileDatasource = new FileDatasource(join(DIR, service + '.bcup'))
     const key = readFileSync(keyPath, 'utf-8')
 
-    ds.save(archive, createCredentials.fromPassword(key))
+    const archive = Archive.createWithDefaults()
+    archive
+      .createGroup('Websites')
+      .createEntry(service)
+      .setProperty('username', creds.username)
+      .setProperty('password', creds.password)
+
+    const credentials = Credentials.fromPassword(key)
+
+    fileDatasource.save(archive.getHistory(), credentials)
       .then((data) => {
         logger.info('Saved new credentials.')
         resolve()
@@ -49,16 +53,32 @@ const setupCreds = (service, credentials) => {
 
 const getCreds = (service) => {
   return new Promise((resolve, reject) => {
-    const ds = new FileDatasource(join(DIR, service + '.bcup'))
+    const fileDatasource = new FileDatasource(join(DIR, service + '.bcup'))
     const key = readFileSync(keyPath, 'utf-8')
 
-    ds.load(createCredentials.fromPassword(key))
+    const credentials = Credentials.fromPassword(key)
+
+    fileDatasource
+      .load(credentials)
+      .then(Archive.createFromHistory)
       .then((archive) => {
-        const finder = new EntryFinder([archive])
-        const res = finder.search('account')[0].entry
-        resolve({
-          username: res.getAttribute('username'),
-          password: res.getAttribute('password')
+        const group = archive.findGroupsByTitle('Websites')[0]
+        const entries = group.getEntries()
+        let hasResolved = false
+
+        // Finding the right entry.
+        // There should be only one entry anyway but you never know
+        entries.forEach((entry) => {
+          const title = entry.getProperty('title')
+
+          if (title === service && !hasResolved) {
+            hasResolved = true
+
+            resolve({
+              username: entry.getProperty('username'),
+              password: entry.getProperty('password')
+            })
+          }
         })
       })
       .catch(/* istanbul ignore next */ (err) => {
