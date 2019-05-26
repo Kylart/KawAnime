cd189ccabc33f0d5d76fff5cc9cf181f853150d4
Kylart <Kylart.dev@gmail.com>
2019-05-26 02:23:40 +0200

    Fixed isAuthed to handle cases where the token has been revoked

--- src/background/server/externals/anilist/auth/isAuthed.js
+++ src/background/server/externals/anilist/auth/isAuthed.js
@@ -1,7 +1,37 @@
 import retrieveToken from './retrieveToken'
+import { graphql } from '../../../utils'
+import { GRAPHQL_ENDPOINT } from '../utils'
+import { search } from '../queries'
+import { setupCreds } from '../../vault'
 
 export default async function () {
-  const data = await retrieveToken()
+  try {
+    const { accessToken, tokenType } = await retrieveToken()
+    const hasToken = !!accessToken && !!tokenType
 
-  return data.accessToken && data.tokenType
+    // Checking if that token if valid
+    if (hasToken) {
+      try {
+        // Obviously we look for Sakura Trick since it's the best anime ever.
+        await graphql(GRAPHQL_ENDPOINT, search, { term: 'Sakura Trick' }, {
+          Authorization: `${tokenType.charAt(0).toUpperCase()}${tokenType.slice(1).toLowerCase()} ${accessToken}`
+        })
+      } catch (e) {
+        // Means that the token is no longer valid, we'll just remove it.
+        setupCreds('anilist', {
+          accessToken: '',
+          tokenType: '',
+          refreshToken: '',
+          expiresAt: ''
+        })
+          .catch((e) => {})
+
+        return false
+      }
+    }
+
+    return hasToken
+  } catch (e) {
+    return false
+  }
 }
