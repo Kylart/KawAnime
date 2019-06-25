2caf50c4ef652bb20d557fff4387f9c70058f3ac
Kylart <kylart.dev@gmail.com>
2019-06-25 15:29:39 +0200

    Added KawAnime secret to authenticate on analytics server

--- src/background/server/services/analytics/index.js
+++ src/background/server/services/analytics/index.js
@@ -1,16 +1,22 @@
+import { readFileSync } from 'fs'
+
 import { eventsList } from '../../../../vendor'
 import { Logger, https } from '../../utils'
+import { localFiles } from '../../externals'
 
 const events = eventsList.analytics
+const secretKey = process.env.VUE_APP_KAWANIME_SECRET
 const ANALYTICS_URL = 'https://kawanime.com/api/v1/analytics'
+const userToken = readFileSync(localFiles.getPath('_token'))
 
 const logger = new Logger('Analytics')
 
 async function send (event, { eventName, data }) {
   https.post(ANALYTICS_URL, {
     eventName,
-    data
-  })
+    data,
+    userToken
+  }, [], { Authorization: secretKey })
     .catch(() => {
       // We ignore errors
       logger.error(`Could not send ${eventName} analytics event.`)
