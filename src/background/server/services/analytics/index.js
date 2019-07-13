0000f9ae6636ad91249f9057d958d31f3ae2367d
Kylart <Kylart.dev@gmail.com>
2019-07-13 15:31:07 +0200

    Added version to send to analytics

--- src/background/server/services/analytics/index.js
+++ src/background/server/services/analytics/index.js
@@ -6,16 +6,18 @@ import { localFiles } from '../../externals'
 
 const events = eventsList.analytics
 const secretKey = process.env.VUE_APP_KAWANIME_SECRET
-const ANALYTICS_URL = 'https://api.kawanime.com/v1/analytics'
+const ANALYTICS_URL = process.env.VUE_APP_KAWANIME_API_URL
+const VERSION = process.env.KAWANIME_VERSION
 const userToken = readFileSync(localFiles.getPath('_token'))
 
 const logger = new Logger('Analytics')
 
 async function send (event, { eventName, data }) {
-  https.post(ANALYTICS_URL, {
+  https.post(`${ANALYTICS_URL}/analytics`, {
     eventName,
     data,
-    userToken
+    userToken,
+    version: VERSION
   }, [], { Authorization: `Bearer ${secretKey}` })
     .catch(() => {
       // We ignore errors
