ef21ce06602a4c5834079f0b54585eaae21058dc
Kylart <Kylart.dev@gmail.com>
2019-09-19 14:10:19 +0200

    Increased maxListeners

--- src/vendor/helpers.js
+++ src/vendor/helpers.js
@@ -5,7 +5,7 @@ import eventsList from './events'
 
 const { ipcRenderer: ipc, remote } = electron
 
-ipc.setMaxListeners(100)
+ipc.setMaxListeners(200)
 
 function log (...args) {
   console.log(`[${(new Date()).toLocaleTimeString()}]:`, ...args) // eslint-disable-line no-console
