ffb4c75aeb9adc5bcb1d514836ac126c504b4ccd
Kylart <kylart.dev@gmail.com>
2019-10-07 18:44:44 +0200

    Created services shorthand helpers

--- src/renderer/store/helpers/index.js
+++ src/renderer/store/helpers/index.js
@@ -1,3 +1,4 @@
 export { default as global } from './global'
 
 export { default as localLists } from './localLists'
+export { default as services } from './services'
---
+++ src/renderer/store/helpers/services.js
@@ -0,0 +1,40 @@
+import { providers } from '../modules/lists'
+
+import createNamespace from './createNamespace'
+
+const PREFIX = '__s'
+const NAMESPACE = 'services'
+
+export default createNamespace(
+  NAMESPACE,
+  PREFIX,
+  {
+    // Brings __sSet, __sHasUser, __sIsConnected, __sGetList, __sUpdateList
+    // and  __sExternal
+    actions: [
+      'set',
+      'hasUser',
+      'isConnected',
+      'getList',
+      'updateList',
+      'external'
+    ],
+    // Brings __sSetUser, __sHasUser, __sSetValue, __sSetConnected, __sSetLoading,
+    // __sSetList, __sSetError, __sShowForm and__sSetFormEntry
+    mutations: [
+      'setUser',
+      'hasUser',
+      'setValue',
+      'setConnected',
+      'setLoading',
+      'setList',
+      'setError',
+      'showForm',
+      'setFormEntry'
+    ],
+    // Brings __s<providerName>
+    state: [
+      ...providers.map(({ value }) => value)
+    ]
+  }
+)
