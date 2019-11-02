a1f3a6053649c8cf50754f1e235f8cb5f678516a
Kylart <Kylart.dev@gmail.com>
2019-11-02 02:37:08 +0100

    Added boost to includes

--- bindings/example.js
+++ bindings/example.js
@@ -1,3 +1,4 @@
-const { parseName } = require('./build/Release/kawabinds')
+const { parseName, torrent } = require('./build/Release/kawabinds')
 
+console.log(torrent)
 console.log(parseName('[Ouroboros]_Fullmetal_Alchemist_Brotherhood_-_01.mkv'))
