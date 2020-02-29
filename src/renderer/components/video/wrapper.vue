ca4d6e07f88c204518094b96f64f28bfdbab49f1
Kylart <Kylart.dev@gmail.com>
2020-02-29 02:59:56 +0100

    Fixed overlay behaviour for player

--- src/renderer/components/video/wrapper.vue
+++ src/renderer/components/video/wrapper.vue
@@ -7,7 +7,7 @@ export default {
       display: 'block',
       width: '100%',
       height: '100%',
-      'pointer-events': 'none' // Allows cursor to be handle directly by MPV?
+      'pointer-events': 'none' // Allows cursor to be handled directly by MPV?
     },
     plugin: null,
     mimeType: 'application/x-mpvjs',
