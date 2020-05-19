68fc6608163c9359d509d2f7daf6341f1c601baa
Kylart <kylart.dev@gmail.com>
2020-05-19 19:08:48 +0200

    Added missing files to dist folder on windows

--- src/main/startUp/register.js
+++ src/main/startUp/register.js
@@ -4,6 +4,7 @@ const isProduction = process.env.NODE_ENV === 'production'
 const isTest = process.env.IS_TEST === 'true'
 
 // Setting up default protocol
+// This will allow the OS to open the app with an URL like kawanime-app://something/otherthing
 if (isProduction && !isTest) {
   app.setAsDefaultProtocolClient(
     'kawanime-app',
